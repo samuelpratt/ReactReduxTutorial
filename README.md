780215c910a98c678686b931c399fe97c95bcfe5
Sam Pratt <samuel.pratt@gmail.com>
2018-11-11 23:15:47 +0000

    Added initial redux state, actions and reducers.

--- README.md
+++ README.md
@@ -12,7 +12,11 @@ npm start
 ## TODO
 
 * ~~Basic project structure (https://hackernoon.com/how-to-build-a-react-project-from-scratch-using-webpack-4-and-babel-56d4a26afd32)~~
-* Add some redux
+* Add some redux (based on https://redux.js.org/basics)
 * Do we need gulp to build?
 * Do we need a CSS pre-processor?
 * Add some unit tests
+
+## Notes
+
+React Chrome tools are here https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App.js"
+
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+  } from "./state/actions"
+
+import { createStore } from 'redux'
+
+
+
+import todoApp from "./state/reducers"
+const store = createStore(todoApp)
+
+// Log the initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
 
 ReactDOM.render(<App />, document.getElementById("root"));
